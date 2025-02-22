ff5cca19599736b49583433b93b0b768e3b4ff0d
josebon2005 <jbonillas1@miumg.edu.gt>
2025-02-22 00:01:03 -0600

    Imprimir la serie especial: 1, 3, 6, 10, 15, ..., n

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <limits>  // Para utilizar numeric_limits
 using namespace std;
 
 // Prototipos de funciones
@@ -8,6 +7,7 @@ void tablaMultiplicar();
 void sumaParesHastaN();
 void calcularFactorial();
 void imprimirPrimos1a50();
+void imprimirSerieEspecial();
 
 int main() {
     int opcion;
@@ -20,6 +20,7 @@ int main() {
         cout << "3. Calcular la suma de todos los numeros pares desde 1 hasta n\n";
         cout << "4. Calcular el factorial de un numero\n";
         cout << "5. Imprimir la serie de numeros primos del 1 al 50\n";
+        cout << "6. Imprimir la serie especial: 1, 3, 6, 10, 15, ..., n\n";
         cout << "0. Salir\n";
         cout << "Seleccione una opcion: ";
         cin >> opcion;
@@ -30,6 +31,7 @@ int main() {
             case 3: sumaParesHastaN(); break;
             case 4: calcularFactorial(); break;
             case 5: imprimirPrimos1a50(); break;
+            case 6: imprimirSerieEspecial(); break;
             case 0: cout << "Saliendo del programa...\n"; break;
             default: cout << "Opcion invalida, intente de nuevo.\n";
         }
@@ -52,11 +54,7 @@ void imprimirPares2a20() {
 void tablaMultiplicar() {
     int num;
     cout << "\nIngrese un numero para mostrar su tabla de multiplicar: ";
-    while (!(cin >> num)) { // Validación de entrada
-        cout << "Entrada no válida. Por favor ingrese un número entero: ";
-        cin.clear(); // Limpiar el error de entrada
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignorar el resto de la entrada incorrecta
-    }
+    cin >> num;
 
     cout << "Tabla del " << num << ":\n";
     for (int i = 1; i <= 10; i++) {
@@ -68,10 +66,12 @@ void tablaMultiplicar() {
 void sumaParesHastaN() {
     int n, suma = 0;
     cout << "\nIngrese un numero entero positivo: ";
-    while (!(cin >> n) || n <= 0) { // Validación de entrada
-        cout << "Entrada no válida. Por favor ingrese un número entero positivo: ";
-        cin.clear(); // Limpiar el error de entrada
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignorar el resto de la entrada incorrecta
+    cin >> n;
+
+    // Validación para asegurarse que 'n' sea positivo
+    while (n <= 0) {
+        cout << "Por favor ingrese un numero mayor que 0: ";
+        cin >> n;
     }
 
     for (int i = 2; i <= n; i += 2) {
@@ -86,11 +86,11 @@ void calcularFactorial() {
     int num;
     long long factorial = 1;
     cout << "\nIngrese un numero para calcular su factorial: ";
+    cin >> num;
 
-    while (!(cin >> num) || num < 0) { // Validación de entrada y números negativos
-        cout << "Entrada no válida o número negativo. Por favor ingrese un número entero no negativo: ";
-        cin.clear(); // Limpiar el error de entrada
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignorar el resto de la entrada incorrecta
+    if (num < 0) {
+        cout << "El factorial no esta definido para numeros negativos.\n";
+        return;
     }
 
     for (int i = 1; i <= num; i++) {
@@ -107,7 +107,7 @@ void imprimirPrimos1a50() {
     for (int num = 2; num <= 50; num++) {
         bool esPrimo = true;
 
-        for (int i = 2; i * i <= num; i++) { // Optimización en la verificación de números primos
+        for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) {
                 esPrimo = false;
                 break;
@@ -119,6 +119,26 @@ void imprimirPrimos1a50() {
         }
     }
 
+    cout << endl;
+}
+
+// Función que imprime la serie 1, 3, 6, 10, 15, ..., n
+void imprimirSerieEspecial() {
+    int n, suma = 0;
+    cout << "\nIngrese un numero para generar la serie: ";
+    cin >> n;
+
+    // Validación para que 'n' sea positivo
+    while (n <= 0) {
+        cout << "Por favor ingrese un numero mayor que 0: ";
+        cin >> n;
+    }
+
+    cout << "Serie: ";
+    for (int i = 1; suma + i <= n; i++) {
+        suma += i;
+        cout << suma << " ";
+    }
     cout << endl;
 }
 // TIP See CLion help at <a
