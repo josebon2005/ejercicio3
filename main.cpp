cc05d58b2e88e86708f8313602894838fcdd23bb
josebon2005 <jbonillas1@miumg.edu.gt>
2025-02-21 23:48:09 -0600

    Mostrar la tabla de multiplicar de un numero ingresado

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <limits> // Para usar numeric_limits
 using namespace std;
 
-// Prototipo de funciones
+// Prototipos de funciones
 void imprimirPares2a20();
+void tablaMultiplicar();
 
 int main() {
     int opcion;
@@ -11,12 +13,14 @@ int main() {
         // Menú de opciones
         cout << "\n--- MENU DE EJERCICIOS (BUCLES FOR) ---\n";
         cout << "1. Imprimir los numeros pares del 2 al 20\n";
+        cout << "2. Mostrar la tabla de multiplicar de un numero ingresado\n";
         cout << "0. Salir\n";
         cout << "Seleccione una opcion: ";
         cin >> opcion;
 
         switch (opcion) {
             case 1: imprimirPares2a20(); break;
+            case 2: tablaMultiplicar(); break;
             case 0: cout << "Saliendo del programa...\n"; break;
             default: cout << "Opcion invalida, intente de nuevo.\n";
         }
@@ -35,6 +39,30 @@ void imprimirPares2a20() {
     cout << endl;
 }
 
+// Función que muestra la tabla de multiplicar de un número ingresado
+void tablaMultiplicar() {
+    int num;
+
+    // Validación de entrada para asegurarse de que el usuario ingrese un número
+    cout << "\nIngrese un numero para mostrar su tabla de multiplicar: ";
+
+    // Asegurarnos de que el usuario ingrese un número válido
+    while (true) {
+        if (cin >> num) {
+            break; // Si la entrada es un número válido, salimos del bucle
+        } else {
+            cout << "Por favor ingrese un numero valido: ";
+            cin.clear(); // Limpiar el error de entrada
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignorar el resto de la entrada incorrecta
+        }
+    }
+
+    cout << "Tabla del " << num << ":\n";
+    for (int i = 1; i <= 10; i++) {
+        cout << num << " x " << i << " = " << (num * i) << endl;
+    }
+    cout << endl; // Agregar un salto de línea al final para mayor claridad
+}
 // TIP See CLion help at <a
 // href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
 //  Also, you can try interactive lessons for CLion by selecting
